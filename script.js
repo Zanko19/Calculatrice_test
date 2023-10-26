534eb7045c8e05974ef922e5e39c2133ab0a981a
Guillaume <guizanko@hotmail.fr>
2023-10-26 16:22:21 +0200

    try

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("test");
+let displayValue = "";
+
+function appendToDisplay(value) {
+  displayValue += value;
+  document.getElementById("display").value = displayValue;
+}
+
+function clearDisplay() {
+  displayValue = "";
+  document.getElementById("display").value = "";
+}
+
+function calculate() {
+  try {
+    const result = new Function('return ' + displayValue)();
+    displayValue = result;
+    document.getElementById("display").value = result;
+  } catch (error) {
+    document.getElementById("display").value = "Error";
+  }
+}
